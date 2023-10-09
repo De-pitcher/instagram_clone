098f25680189ba934997f89b236df0a80d3b05e4
De-pitcher <emmanwa000@gmail.com>
2023-10-09 04:00:35 +0100

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(body: Text('This is the first code I am writing in the instagram clone')),
+    );
+  }
+}
