1510612265959714dc9fd0d8f214f3917978f1b9
De-pitcher <emmanwa000@gmail.com>
2023-10-23 04:13:11 +0100

    ADDED SEARCH SCREEN

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 import '../screens/add_post_screen.dart';
 import '../screens/feed_screen.dart';
+import '../screens/search_screen.dart';
 
 const webScreensize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('Search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('Favorite'),
   Text('Profile'),
